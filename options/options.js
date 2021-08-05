41bec64ae3ae74ea874ce679a7b1e118ebd38d95
Crowdbotics <team@crowdbotics.com>
2021-08-05 15:20:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wispy_dew_29511Identifier",
+  url: "https://wispy_dew_29511Identifier.botics.co",
+  api: "https://wispy_dew_29511Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
